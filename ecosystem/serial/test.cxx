ec1c6ffd923786cba4db629059608306ca598486
Brandon White <b.white.1@pm.me>
2021-09-08 16:05:37 -0700

    Update serial test.

--- ecosystem/serial/test.cxx
+++ ecosystem/serial/test.cxx
@@ -5,30 +5,44 @@
 
 #include "serial.h"
 
-const auto comm_port = "\\\\.\\COM5";
-const auto comm_baud_rate = 9600;
+const auto comm_port = "\\\\.\\COM7";
 
 int main() {
     spdlog::set_level(spdlog::level::debug);
     sc::serial::comm_instance comm;
     spdlog::debug("{}", reinterpret_cast<void *>(&comm));
-    if (const auto err = comm.open(comm_port, comm_baud_rate); err) {
+    if (const auto err = comm.open(comm_port); err) {
         spdlog::error(*err);
         return 1;
     }
+    std::vector<std::byte> message;
+    message.push_back(static_cast<std::byte>(0x69));
     for (int i = 0; i < 3; i++) {
         spdlog::info("Starting iteration #{}.", i + 1);
-        spdlog::info("Waiting 4 seconds...");
-        std::this_thread::sleep_for(std::chrono::seconds(4));
-        const auto res = comm.read();
-        if (!res.has_value()) {
-            spdlog::error(res.error());
+        if (const auto err = comm.write(message); err) {
+            spdlog::error("Unable to send message.");
             return 2;
         }
-        spdlog::info("Got {} bytes.", res->size());
+        spdlog::info("Message sent. ({} bytes)", message.size());
+        spdlog::info("Waiting 1 seconds...");
+        std::vector<std::byte> buffer;
+        while (buffer.size() < 21) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(2));
+            const auto res = comm.read();
+            if (!res.has_value()) {
+                spdlog::error(res.error());
+                return 3;
+            }
+            if (res->size() == 0) continue;
+            spdlog::debug("Got {} bytes...", res->size());
+            buffer.insert(buffer.begin(), res->begin(), res->end());
+        }
+        spdlog::info("Got {} bytes in total.", buffer.size());
         fmt::print("\n");
-        for (auto &b : *res) fmt::print("{}", static_cast<char>(b));
-        fmt::print("\n{}\n\n", spdlog::to_hex(res->begin(), res->end()));
+        for (auto &b : buffer) fmt::print("{}", static_cast<char>(b));
+        fmt::print("\n{}\n\n", spdlog::to_hex(buffer.begin(), buffer.end()));
+        if (strncmp("sim_coaches_hardware;", reinterpret_cast<const char *>(buffer.data()), 21) == 0) spdlog::info("Sim Coaches hardware identified.");
+        else spdlog::warn("Not Sim Coaches hardware.");
     }
     comm.close();
     spdlog::info("Test complete.");
