857725c5f4bc660503f6d6358f8c969a5dad6679
Brandon White <b.white.1@pm.me>
2021-09-15 11:21:21 -0700

    Simplify Lottie build.
    Assume static.

--- ecosystem/precompiled/rlottie/include/rlottiecommon.h
+++ ecosystem/precompiled/rlottie/include/rlottiecommon.h
@@ -23,19 +23,7 @@
 #ifndef _RLOTTIE_COMMON_H_
 #define _RLOTTIE_COMMON_H_
 
-#if defined _WIN32 || defined __CYGWIN__
-  #ifdef RLOTTIE_BUILD
-    #define RLOTTIE_API __declspec(dllexport)
-  #else
-    #define RLOTTIE_API __declspec(dllimport)
-  #endif
-#else
-  #ifdef RLOTTIE_BUILD
-      #define RLOTTIE_API __attribute__ ((visibility ("default")))
-  #else
-      #define RLOTTIE_API
-  #endif
-#endif
+#define RLOTTIE_API
 
 
 /**
