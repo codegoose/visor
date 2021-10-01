4ba4a31297d7b3e39b4cfc9d99428b578a98a81d
Brandon White <b.white.1@pm.me>
2021-10-01 09:54:55 -0700

    Improve thread safety.

--- ecosystem/application/gui.cxx
+++ ecosystem/application/gui.cxx
@@ -6,6 +6,8 @@
 #include <optional>
 #include <functional>
 #include <future>
+#include <mutex>
+#include <memory>
 #include <imgui.h>
 #include <fmt/format.h>
 #include <glm/common.hpp>
@@ -95,38 +97,39 @@ namespace sc::visor::gui {
         std::future<std::optional<std::string>> update_future;
         std::atomic_bool initial_communication_complete = false;
 
-        std::optional<std::string> update() {
+        static std::optional<std::string> update(std::shared_ptr<device_context> context) {
+            if (!context || !context->handle) return std::nullopt;
             {
-                std::lock_guard guard(mutex);
+                std::lock_guard guard(context->mutex);
                 const auto now = std::chrono::high_resolution_clock::now();
-                if (!last_communication) last_communication = now;
-                if (std::chrono::duration_cast<std::chrono::milliseconds>(now - *last_communication).count() < 2) return std::nullopt;
+                if (!context->last_communication) context->last_communication = now;
+                if (std::chrono::duration_cast<std::chrono::milliseconds>(now - *context->last_communication).count() < 2) return std::nullopt;
             }
             DEFER({
-                std::lock_guard guard(mutex);
-                last_communication = std::chrono::high_resolution_clock::now();
+                std::lock_guard guard(context->mutex);
+                context->last_communication = std::chrono::high_resolution_clock::now();
             });
-            if (const auto res = handle->get_version(); res.has_value()) {
-                version_major = std::get<0>(*res);
-                version_minor = std::get<1>(*res);
-                version_revision = std::get<2>(*res);
+            if (const auto res = context->handle->get_version(); res.has_value()) {
+                context->version_major = std::get<0>(*res);
+                context->version_minor = std::get<1>(*res);
+                context->version_revision = std::get<2>(*res);
             } else return res.error();
-            const auto axes_res = handle->get_num_axes();
+            const auto axes_res = context->handle->get_num_axes();
             if (!axes_res.has_value()) return axes_res.error();
-            mutex.lock();
-            axes.resize(*axes_res);
-            axes_ex.resize(axes.size());
-            mutex.unlock();
+            context->mutex.lock();
+            context->axes.resize(*axes_res);
+            context->axes_ex.resize(context->axes.size());
+            context->mutex.unlock();
             for (int i = 0; i < *axes_res; i++) {
-                const auto res = handle->get_axis_state(i);
+                const auto res = context->handle->get_axis_state(i);
                 if (!res.has_value()) return res.error();
-                if (!initial_communication_complete) {
-                    axes_ex[i].range_min = res->min;
-                    axes_ex[i].range_max = res->max;
+                if (!context->initial_communication_complete) {
+                    context->axes_ex[i].range_min = res->min;
+                    context->axes_ex[i].range_max = res->max;
                 }
-                axes[i] = *res;
+                context->axes[i] = *res;
             }
-            initial_communication_complete = true;
+            context->initial_communication_complete = true;
             return std::nullopt;
         }
     };
@@ -242,7 +245,7 @@ namespace sc::visor::gui {
                 } else continue;
             }
             context->update_future = async(std::launch::async, [context]() {
-                return context->update();
+                return device_context::update(context);
             });
         }
     }
