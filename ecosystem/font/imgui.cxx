3e3274cb009c822eb848f9711147fbb34e86bf59
Brandon White <b.white.1@pm.me>
2021-10-06 14:45:38 -0700

    Upgrade font icons.

--- ecosystem/font/imgui.cxx
+++ ecosystem/font/imgui.cxx
@@ -8,7 +8,7 @@ bool sc::font::imgui::load(const int &size) {
     {
         ImFontConfig font_config;
         font_config.FontDataOwnedByAtlas = false;
-        if (static const auto rsc_font_1 = resource::get_resource("DATA", "FONT_1"); !rsc_font_1 || !ImGui::GetIO().Fonts->AddFontFromMemoryTTF(rsc_font_1->first, rsc_font_1->second, size, &font_config)) {
+        if (static const auto rsc_font_1 = resource::get_resource("DATA", "FONT_DEFAULT"); !rsc_font_1 || !ImGui::GetIO().Fonts->AddFontFromMemoryTTF(rsc_font_1->first, rsc_font_1->second, size, &font_config)) {
             spdlog::error("Unable to load primary font.");
             return false;
         }
@@ -19,7 +19,7 @@ bool sc::font::imgui::load(const int &size) {
         icons_config.MergeMode = true;
         icons_config.PixelSnapH = true;
         icons_config.FontDataOwnedByAtlas = false;
-        if (static const auto rsc_font_2 = resource::get_resource("DATA", "FONT_2"); !rsc_font_2 || !ImGui::GetIO().Fonts->AddFontFromMemoryTTF(rsc_font_2->first, rsc_font_2->second, size - 4, &icons_config, icons_ranges)) {
+        if (static const auto rsc_font_2 = resource::get_resource("DATA", "FONT_FA_SOLID"); !rsc_font_2 || !ImGui::GetIO().Fonts->AddFontFromMemoryTTF(rsc_font_2->first, rsc_font_2->second, size - 4, &icons_config, icons_ranges)) {
             spdlog::error("Unable to load secondary font.");
             return false;
         }
@@ -30,7 +30,7 @@ bool sc::font::imgui::load(const int &size) {
         icons_config.MergeMode = true;
         icons_config.PixelSnapH = true;
         icons_config.FontDataOwnedByAtlas = false;
-        if (static const auto rsc_font_3 = resource::get_resource("DATA", "FONT_3"); !rsc_font_3 || !ImGui::GetIO().Fonts->AddFontFromMemoryTTF(rsc_font_3->first, rsc_font_3->second, size - 4, &icons_config, icons_ranges)) {
+        if (static const auto rsc_font_3 = resource::get_resource("DATA", "FONT_FA_BRANDS"); !rsc_font_3 || !ImGui::GetIO().Fonts->AddFontFromMemoryTTF(rsc_font_3->first, rsc_font_3->second, size - 4, &icons_config, icons_ranges)) {
             spdlog::error("Unable to load tertiary font.");
             return false;
         }
