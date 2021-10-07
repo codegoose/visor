56abbf858d9ea76d78ba6783280122544e7b4dac
Brandon White <b.white.1@pm.me>
2021-10-07 12:16:30 -0700

    Bump version.

--- ecosystem/application/version.h
+++ ecosystem/application/version.h
@@ -1,6 +1,6 @@
 // There NEEDS to be a new line at the end of this file.
 
 #define SC_APP_NAME "Visor"
-#define SC_APP_VER "2.0.7.200" // Windows manifest block wants this format.
-#define SC_FILE_VER 2,0,7,200 // Windows manifest wants this format.
+#define SC_APP_VER "2.0.9.300" // Windows manifest block wants this format.
+#define SC_FILE_VER 2,0,9,300 // Windows manifest wants this format.
 #define SC_SENTRY_DSN "https://7b03cf44ee134c5c88aba0d89b233142@o881067.ingest.sentry.io/5972590"
