b803deefe9448730494303f40158c7a98d255736
Brandon White <b.white.1@pm.me>
2021-10-29 14:26:17 -0700

    Update file functions.

--- libs/file/file.cxx
+++ libs/file/file.cxx
@@ -4,7 +4,7 @@
 
 tl::expected<std::vector<std::byte>, std::string> sc::file::load(const std::filesystem::path &path) {
     std::ifstream ifs(path, std::ios::binary | std::ios::ate);
-    if (!ifs) return tl::make_unexpected(std::strerror(errno));
+    if (!ifs) return tl::make_unexpected("Unable to open file.");
     const auto end = ifs.tellg();
     ifs.seekg(0, std::ios::beg);
     const auto size = end - ifs.tellg();
@@ -12,4 +12,12 @@ tl::expected<std::vector<std::byte>, std::string> sc::file::load(const std::file
     std::vector<std::byte> buffer(size);
     if (!ifs.read(reinterpret_cast<char *>(buffer.data()), buffer.size())) return tl::make_unexpected(std::strerror(errno));
     return std::move(buffer);
+}
+
+std::optional<std::string> sc::file::save(const std::filesystem::path &path, const std::vector<std::byte> &data) {
+    std::ofstream ofs(path, std::ios::binary | std::ios::trunc);
+    if (!ofs) return "Unable to open file.";
+    ofs.write(reinterpret_cast<const char *>(data.data()), data.size());
+    if (!ofs) return "Unable to write data.";
+    return std::nullopt;
 }
--- libs/file/file.h
+++ libs/file/file.h
@@ -4,10 +4,12 @@
 #include <vector>
 #include <string>
 #include <filesystem>
+#include <optional>
 
 #include <tl/expected.hpp>
 
 namespace sc::file {
 
-    tl::expected<std::vector<std::byte>, std::string> load(const std::filesystem::path &path); 
+    tl::expected<std::vector<std::byte>, std::string> load(const std::filesystem::path &path);
+    std::optional<std::string> save(const std::filesystem::path &path, const std::vector<std::byte> &data);
 }
