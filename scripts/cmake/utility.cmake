e458a79736982b19374bd5dff0ced7d89bc8379f
Brandon White <b.white.1@pm.me>
2021-12-17 10:20:14 -0800

    Remove CMake admin function.
    Doesn't work within Ninja.

--- scripts/cmake/utility.cmake
+++ scripts/cmake/utility.cmake
@@ -4,18 +4,6 @@ function(win32_disable_default_manifest)
     endif()
 endfunction()
 
-
-function(win32_require_admin target)
-    if(WIN32)
-        add_custom_command(
-            TARGET ${target}
-            POST_BUILD
-            COMMAND "mt.exe" -manifest \"$(SolutionDir)..\\..\\..\\apps\\need_admin.manifest\" -outputresource:"$(TargetDir)$(TargetFileName)"\;\#1
-            COMMENT "Adding admin manifest to '$(TargetFileName)' ..." 
-        )
-    endif()
-endfunction()
-
 function(win32_release_mode_no_console target)
     if(WIN32)
         if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
