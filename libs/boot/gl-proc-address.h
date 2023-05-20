a9348d6682c9f09edfc3c1893858474ca6fd6b53
SimCoaches <98986500+SimCoaches@users.noreply.github.com>
2023-05-19 22:47:53 -0700

    Update gl-proc-address.h
    added comments

--- libs/boot/gl-proc-address.h
+++ libs/boot/gl-proc-address.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <functional>
+#include <functional> // Include the library for function objects
 
-#include <glbinding/glbinding.h>
+#include <glbinding/glbinding.h> // Include the glbinding library for OpenGL bindings
 
 namespace sc::boot::gl {
 
-    extern std::function<glbinding::ProcAddress(const char*)> get_proc_address;
+    extern std::function<glbinding::ProcAddress(const char*)> get_proc_address; // Declare an external function object named "get_proc_address"
 }
