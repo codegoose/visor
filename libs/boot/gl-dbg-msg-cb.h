5f734831b2a04dbc3502256e021d6abff03fda76
SimCoaches <98986500+SimCoaches@users.noreply.github.com>
2023-05-19 22:42:10 -0700

    Update gl-dbg-msg-cb.h
    added comments

--- libs/boot/gl-dbg-msg-cb.h
+++ libs/boot/gl-dbg-msg-cb.h
@@ -1,10 +1,12 @@
-#pragma once
+#pragma once  // Ensures this header file is included only once during compilation.
 
-#include <glbinding/gl33core/gl.h>
+#include <glbinding/gl33core/gl.h>  // Includes the necessary OpenGL header file.
 
-using namespace gl;
+using namespace gl;  // Brings the gl namespace into the current scope for convenience.
 
-namespace sc::boot::gl {
+namespace sc::boot::gl {  // Starts a new namespace block for sc::boot::gl.
 
     void debug_message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *user);
-}
+    // Declares the debug_message_callback function with the specified parameter types.
+
+}  // End of namespace sc::boot::gl.
