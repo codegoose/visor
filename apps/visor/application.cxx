d2a8bd1a82207972821a0f99b8ebfe88e9a1b078
SimCoaches <98986500+SimCoaches@users.noreply.github.com>
2023-05-19 19:30:15 -0700

    Update application.cxx
    Added comments for better understanding

--- apps/visor/application.cxx
+++ apps/visor/application.cxx
@@ -1,5 +1,11 @@
 #include "application.h"
+// This includes the "application.h" header file, which likely contains definitions and declarations for the Application class or functions related to application management.
 
 std::atomic_bool sc::visor::keep_running = true;
+// This initializes the 'keep_running' atomic boolean variable in the sc::visor namespace. Atomic variables are used to guarantee that operations on them are not interrupted or manipulated concurrently by different threads. This variable is likely used to control a loop or other ongoing process, with the process continuing as long as 'keep_running' is true.
+
 bool sc::visor::legacy_support_error = false;
+// This initializes the 'legacy_support_error' boolean variable in the sc::visor namespace to false. This flag is likely used to indicate if an error occurred related to legacy support. By default, it's set to false, meaning no error has occurred.
+
 std::optional<std::string> sc::visor::legacy_support_error_description;
+// This initializes the 'legacy_support_error_description' optional string variable in the sc::visor namespace. Optional variables can contain a value or be empty (nullopt). This variable is likely used to contain a description of an error related to legacy support, if such an error occurred. By default, it's empty, meaning no error description is provided.
