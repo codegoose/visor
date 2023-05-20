99459edaab42c09af740c5d3af66547706c0c664
SimCoaches <98986500+SimCoaches@users.noreply.github.com>
2023-05-19 22:13:42 -0700

    Update version.cxx
    Added comments

--- apps/visor/version.cxx
+++ apps/visor/version.cxx
@@ -1,6 +1,6 @@
-#include "main.rc"
+#include "main.rc" // Include the resource file for the main application
 
-#include "../../libs/version/version.h"
+#include "../../libs/version/version.h" // Include the version header file from the library
 
-const std::string sc::version::app_name = VER_APP_NAME;
-const std::string sc::version::app_ver = VER_APP_VER;
+const std::string sc::version::app_name = VER_APP_NAME; // Define the application name using the version information
+const std::string sc::version::app_ver = VER_APP_VER; // Define the application version using the version information
