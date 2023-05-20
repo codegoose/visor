c6f49523ab99d0718ebf7a19fc922e63d971fd92
SimCoaches <98986500+SimCoaches@users.noreply.github.com>
2023-05-19 22:40:36 -0700

    Update gl-dbg-msg-cb.cxx
    Added comments

--- libs/boot/gl-dbg-msg-cb.cxx
+++ libs/boot/gl-dbg-msg-cb.cxx
@@ -1,19 +1,28 @@
-#include "gl-dbg-msg-cb.h"
-
-#include <sentry.h>
-#include <spdlog/spdlog.h>
-
+// Define a callback function named "debug_message_callback" which is used as the debug message callback for OpenGL
 void sc::boot::gl::debug_message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *user) {
     if (severity == GL_DEBUG_SEVERITY_HIGH) {
+        // If the severity of the debug message is high, log an error message using the "spdlog" library
         spdlog::error("GL Error: {}", message);
+        // Flush the log to ensure the error message is written immediately
         spdlog::default_logger()->flush();
+        // Create a Sentry event with the error message and capture it
         const auto sentry_event = sentry_value_new_message_event(SENTRY_LEVEL_ERROR, "OpenGL", message);
         sentry_event_value_add_stacktrace(sentry_event, nullptr, 0);
         sentry_capture_event(sentry_event);
-    } else if (severity == GL_DEBUG_SEVERITY_MEDIUM) spdlog::warn("GL Error: {}", message);
-    else if (severity == GL_DEBUG_SEVERITY_LOW) spdlog::debug("GL Error: {}", message);
-    else {
-        if (strstr(message, "GL_STREAM_DRAW") || strstr(message, "GL_STATIC_DRAW")) return; // yeh i know pls stop
+    } else if (severity == GL_DEBUG_SEVERITY_MEDIUM) {
+        // If the severity of the debug message is medium, log a warning message
+        spdlog::warn("GL Error: {}", message);
+    } else if (severity == GL_DEBUG_SEVERITY_LOW) {
+        // If the severity of the debug message is low, log a debug message
+        spdlog::debug("GL Error: {}", message);
+    } else {
+        // If the severity of the debug message is not high, medium, or low,
+        // check if the message contains "GL_STREAM_DRAW" or "GL_STATIC_DRAW".
+        // If so, return and do nothing.
+        if (strstr(message, "GL_STREAM_DRAW") || strstr(message, "GL_STATIC_DRAW"))
+            return; // Skip processing for known irrelevant messages
+
+        // Log a debug message for other notifications
         spdlog::debug("GL Notification: {}", message);
     }
 }
