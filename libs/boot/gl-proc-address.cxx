cb3d30c1b2a3cb736b83a1ec3cf8a2a8e54acfbd
SimCoaches <98986500+SimCoaches@users.noreply.github.com>
2023-05-19 22:46:45 -0700

    Update gl-proc-address.cxx
    Added Comments

--- libs/boot/gl-proc-address.cxx
+++ libs/boot/gl-proc-address.cxx
@@ -1,19 +1,26 @@
-#include <spdlog/spdlog.h>
-#include <glbinding/gl33core/gl.h>
-#include <GLFW/glfw3.h>
+#include <spdlog/spdlog.h> // Include the library for logging
+#include <glbinding/gl33core/gl.h> // Include the OpenGL binding library
+#include <GLFW/glfw3.h> // Include the GLFW library for windowing
 
-#include "gl-proc-address.h"
+#include "gl-proc-address.h" // Include the header file for getting the OpenGL function address
 
-using namespace gl;
+using namespace gl; // Use the gl namespace from the glbinding library
 
+// Define a function named "get_proc_address" with a specific signature and implementation
 std::function<glbinding::ProcAddress(const char*)> sc::boot::gl::get_proc_address = [](const char *name) -> glbinding::ProcAddress {
+    // Get the address of the OpenGL function using GLFW's getProcAddress
     const auto addr = glfwGetProcAddress(name);
+    // Create a text with the function name and its address
     const auto text = fmt::format("GL: {} @ {}", name, reinterpret_cast<void *>(addr));
-    if (addr) spdlog::debug(text);
-    else {
+    if (addr) {
+        // If the address is valid, log a debug message with the text
+        spdlog::debug(text);
+    } else {
+        // If the address is invalid, log a warning message with the text, flush the log, and exit with code 1000
         spdlog::warn(text);
         spdlog::default_logger()->flush();
         exit(1000);
     }
+    // Return the address as a glbinding ProcAddress
     return reinterpret_cast<glbinding::ProcAddress>(addr);
 };
