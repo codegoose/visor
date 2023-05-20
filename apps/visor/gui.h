0b9b657ed1fe7df77b0584e555e2fc95d226de34
SimCoaches <98986500+SimCoaches@users.noreply.github.com>
2023-05-19 21:58:22 -0700

    Update gui.h
    updated with comments

--- apps/visor/gui.h
+++ apps/visor/gui.h
@@ -1,24 +1,25 @@
-#pragma once
+#pragma once // Include guard to ensure this header is included only once
 
-#include <glm/vec2.hpp>
+#include <glm/vec2.hpp> // Include the glm library's vec2.hpp header
 
-#include <string>
-#include <string_view>
-#include <functional>
-#include <optional>
+#include <string> // Include the string header
+#include <string_view> // Include the string_view header
+#include <functional> // Include the functional header
+#include <optional> // Include the optional header
 
 namespace sc::visor::gui {
 
-    struct popup {
+    struct popup { // Declare a struct named "popup"
 
-        std::string label;
-        glm::ivec2 size;
-        std::function<void()> emit;
+        std::string label; // A string member variable to store the label of the popup
+        glm::ivec2 size; // A glm::ivec2 member variable to store the size of the popup
+        std::function<void()> emit; // A function member variable to store a callable object that takes no arguments and returns void
 
-        void launch();
+        void launch(); // Declare a member function named "launch" that launches the popup
     };
 
-    void initialize();
-    void shutdown();
+    void initialize(); // Declare a global function named "initialize" that initializes the GUI
+    void shutdown(); // Declare a global function named "shutdown" that shuts down the GUI
     void emit(const glm::ivec2 &framebuffer_size, bool *const force_redraw = nullptr);
+    // Declare a global function named "emit" that takes the framebuffer size as a glm::ivec2 reference and an optional pointer to a bool, and triggers an event
 }
